0bc7e487a511770560bb4533ccd6e8b35df498a4
Richie Bendall <richiebendall@gmail.com>
2019-11-26 16:55:14 +1300

    refactor: Simplify code
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
 "use strict"
 
 const Chance = require("chance")
-const fetch = require("cross-fetch")
 const { Random, nativeMath, browserCrypto, nodeCrypto } = require("random-js")
-const { isBrowser, isNode } = require("browser-or-node")
-const { stringify: encodeQueryString } = require("query-string")
+const envCrosser = require("env-crosser")
 const Mutatable = require("mutatable")
+const Rdo = require("rdo")
 
+const rnd = new Rdo()
 const randomizer = new Mutatable()
 
 // OFFLINE: Get crypto strategy
-let gen
-if (isNode) gen = nodeCrypto
-else if (isBrowser && window && window.crypto) gen = browserCrypto
-else gen = nativeMath
+const browCrypt = window && window.crypto ? browserCrypto : undefined
+const gen = envCrosser({
+    node: nodeCrypto,
+    browser: browCrypt,
+    worker: browCrypt,
+    fallback: nativeMath,
+})
 
 // OFFLINE: Create generator
 const num = new Random(gen).integer(-0x20000000000000, 0x20000000000000)
 randomizer.exportable = new Chance(num)
 
 // ONLINE: Get randomizer seed
-fetch(`https://www.random.org/integers/?${encodeQueryString({
-    num: 1,
-    col: 1,
-    min: 1,
-    max: 1000000000,
-    base: 10,
-    format: "plain",
-    rnd: "new",
-})}`)
-    .then((res) => res.text())
+rnd.integer({
+    min: -1e9,
+    max: 1e9,
+})
     .then((res) => randomizer.exportable = new Chance(res))
     .catch(() => { })
 
